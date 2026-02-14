28df1bd2bc1d55111ca35b337f91449692b8e367
Jochen Neubeck <jochen.neubeck.ext@siemens-energy.com>
2026-02-14 10:40:39 +0100

    Follow https://superuser.com/questions/1244934 to reduce visual effect bloat and tweak Windows Explorer behavior

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,8 +6,53 @@ reg add "HKEY_CURRENT_USER\\Keyboard Layout\\Preload" -v 1 -t REG_SZ -d $KEYBOAR
 # Allow to suspend updates for up to 1 year
 reg add "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\WindowsUpdate\\UX\\Settings" -v FlightSettingsMaxPauseDays -t REG_DWORD -d 365 -f
 
-# Follow https://superuser.com/questions/1052763 to reduce the over-animating
-reg add "HKEY_CURRENT_USER\\Control Panel\\Desktop\\WindowMetrics" -v MinAnimate -t REG_SZ -d 0 -f
+# Follow https://superuser.com/questions/1244934 to reduce visual effect bloat and tweak Windows Explorer behavior
+# 0 = Let Windows choose what’s best for my computer
+# 1 = Adjust for best appearance
+# 2 = Adjust for best performance
+# 3 = Custom
+reg add "HKCU\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\VisualEffects" -v "VisualFXSetting" -t REG_DWORD -d 3 -f
+# This disables the following 8 settings:
+# - Animate controls and elements inside windows
+# - Fade or slide menus into view
+# - Fade or slide ToolTips into view
+# - Fade out menu items after clicking
+# - Show shadows under mouse pointer
+# - Show shadows under windows
+# - Slide open combo boxes
+# - Smooth-scroll list boxes
+reg add "HKCU\\Control Panel\\Desktop" -v "UserPreferencesMask" -t REG_BINARY -d "90 12 03 80 10 00 00 00" -f
+# Open File Explorer to: This PC
+reg add "HKCU\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced" -v "LaunchTo" -t REG_DWORD -d 1 -f
+# Show hidden files, folders, and drives
+reg add "HKCU\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced" -v "Hidden" -t REG_DWORD -d 1 -f
+# Hide protected operating system files (Recommended)
+reg add "HKCU\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced" -v "ShowSuperHidden" -t REG_DWORD -d 0 -f
+reg add "HKCU\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced" -v "HideIcons" -t REG_DWORD -d 0 -f
+# Hide extensions for known file types
+reg add "HKCU\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced" -v "HideFileExt" -t REG_DWORD -d 0 -f
+# Navigation pane: Expand to open folder
+reg add "HKCU\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced" -v "NavPaneExpandToCurrentFolder" -t REG_DWORD -d 1 -f
+# Navigation pane: Show all folders
+reg add "HKCU\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced" -v "NavPaneShowAllFolders" -t REG_DWORD -d 1 -f
+# Animate windows when minimizing and maximizing
+reg add "HKCU\\Control Panel\\Desktop\\WindowMetrics" -v "MinAnimate" -d 0 -f
+# Animations in the taskbar
+reg add "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced" -v "TaskbarAnimations" -t REG_DWORD -d 0 -f
+# Enable Peek
+reg add "HKCU\\Software\\Microsoft\\Windows\\DWM" -v "EnableAeroPeek" -t REG_DWORD -d 0 -f
+# Save taskbar thumbnail previews
+reg add "HKCU\\Software\\Microsoft\\Windows\\DWM" -v "AlwaysHibernateThumbnails" -t REG_DWORD -d 0 -f
+# Show translucent selection rectangle
+reg add "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced" -v "ListviewAlphaSelect" -t REG_DWORD -d 0 -f
+# Show thumbnails instead of icons
+reg add "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced" -v "IconsOnly" -t REG_DWORD -d 0 -f
+# Show window contents while dragging
+reg add "HKCU\\Control Panel\\Desktop" -v "DragFullWindows" -d 0 -f
+# Smooth edges of screen fonts
+reg add "HKCU\\Control Panel\\Desktop" -v "FontSmoothing" -d 2 -f
+# Use drop shadows for icon labels on the desktop
+reg add "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced" -v "ListviewShadow" -t REG_DWORD -d 0 -f
 
 # Extend the evaluation period if it did not yet happen
 if $WINDIR/System32/cscript $WINDIR/System32/slmgr.vbs -dlv | findstr -BEC:"Remaining Windows rearm count: 4"; then
